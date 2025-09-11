d86bab3f2099ea2c988f0d876d2104be9e290c08
LeonDellaMea <leonardo.dellamea@edu.unige.it>
2025-09-11 16:48:38 +0200

    Update Inference_object.py

--- Inference_object.py
+++ Inference_object.py
@@ -1,3 +1,9 @@
+"""
+Created on Thu Sep 11 10:54:10 2025
+
+@author: Admin
+"""
+
 # -*- coding: utf-8 -*-
 """
 Created on Fri Sep  5 08:40:59 2025
@@ -153,7 +159,9 @@ Procedure:
     
     
     
+TODO:
     
+    Debug Smoothing CUmulative
     
     
     
@@ -176,64 +184,77 @@ from sbi.utils import BoxUniform
 
 from sbi.inference import NPE, NRE
 from sbi.analysis import pairplot
-# Define the prior
-num_dims = 2
-num_sims = 3000
-num_rounds = 3
-prior = BoxUniform(low=torch.zeros(num_dims), high=torch.ones(num_dims))
-simulator = lambda theta: theta + torch.randn_like(theta) * 0.01
-x_o = torch.tensor([0.5, 0.5])
 
 
-# Generate some samples:
-theta = prior.sample((num_sims,))
-x = simulator(theta)
+import matplotlib.pyplot as plt
+from brian2 import *
+
+from brian2 import clear_cache
 
 
-prior = BoxUniform(torch.zeros(2), torch.ones(2))
-
-from sbi.neural_nets import posterior_nn
-
-density_estimator_build_fun = posterior_nn(
-    model="zuko_nsf", hidden_features=60, num_transforms=10
-)
+import os
 
 
+os.chdir(r'C:\Users\Admin\Desktop\Leonardo\ASN')
+import ASN_fun
+# # Define the prior
+# num_dims = 2
+# num_sims = 3000
+# num_rounds = 3
+# prior = BoxUniform(low=torch.zeros(num_dims), high=torch.ones(num_dims))
+# simulator = lambda theta: theta + torch.randn_like(theta) * 0.01
+# x_o = torch.tensor([0.5, 0.5])
+
+
+# # Generate some samples:
+# theta = prior.sample((num_sims,))
+# x = simulator(theta)
+
+
+# prior = BoxUniform(torch.zeros(2), torch.ones(2))
+
+# from sbi.neural_nets import posterior_nn
+
+# density_estimator_build_fun = posterior_nn(
+#     model="zuko_nsf", hidden_features=60, num_transforms=10
+# )
 
 
 
-inference = NPE(prior=prior, density_estimator=density_estimator_build_fun)
-proposal = prior
 
-# Sequential approach:
+
+# inference = NPE(prior=prior, density_estimator=density_estimator_build_fun)
+# proposal = prior
+
+# # Sequential approach:
     
-for _ in range(num_rounds):
-    theta = proposal.sample((num_sims,))
-    x = simulator(theta)
-    _ = inference.append_simulations(theta, x).train(force_first_round_loss=True)
-    posterior = inference.build_posterior().set_default_x(x_o)
+# for _ in range(num_rounds):
+#     theta = proposal.sample((num_sims,))
+#     x = simulator(theta)
+#     _ = inference.append_simulations(theta, x).train(force_first_round_loss=True)
+#     posterior = inference.build_posterior().set_default_x(x_o)
 
-    accept_reject_fn = get_density_thresholder(posterior, quantile=1e-4)
-    proposal = RestrictedPrior(prior, accept_reject_fn, sample_with="rejection")
-
-
-#%%
-%matplotlib
-x_o = torch.tensor([0.5, 0.5])
-print(f"Shape of x_o: {x_o.shape}            # Must have a batch dimension")
-
-samples = posterior.sample((1000,), x=x_o)
-print(f"Shape of samples: {samples.shape}  # Samples are returned with a batch dimension.")
-
-samples = samples.squeeze(dim=1)
-print(f"Shape of samples: {samples.shape}     # Removed batch dimension.")
-fig, ax = pairplot(
-    samples, limits=[[0, 1], [0, 1]], figsize=(5, 5)
-)
+#     accept_reject_fn = get_density_thresholder(posterior, quantile=1e-4)
+#     proposal = RestrictedPrior(prior, accept_reject_fn, sample_with="rejection")
 
 
+# #%
+# %matplotlib
+# x_o = torch.tensor([0.5, 0.5])
+# print(f"Shape of x_o: {x_o.shape}            # Must have a batch dimension")
 
-def Network_wrapper():
+# samples = posterior.sample((1000,), x=x_o)
+# print(f"Shape of samples: {samples.shape}  # Samples are returned with a batch dimension.")
+
+# samples = samples.squeeze(dim=1)
+# print(f"Shape of samples: {samples.shape}     # Removed batch dimension.")
+# fig, ax = pairplot(
+#     samples, limits=[[0, 1], [0, 1]], figsize=(5, 5)
+# )
+
+
+
+def Network_wrapper(params):
     
     '''
     This wrapper simulates the neuronal and astrocytic network based on the parameters given 
@@ -242,58 +263,69 @@ def Network_wrapper():
     Parameters changed are the ones that we belive are involved in the phenotype we try to 
     reproduce.
     
+    Params:
+        
+        # Neuron
+        0: Sigma [mV]
+        1: g_AHP  (*msiemens*cm**-2) * Neuron_area
+        2: delta 
+        3: I_inj [pA] # DIffernet way to change the variable because it is internally initialized
+        
+        # Synapse
+        4: Xi_ampa [1/mmole]
+        5: Xi_nmda [1/mmole]
+        6: Y_T  [mmole]
+        7: conn_prob
+        8: Omega_d [1/second]
+        9: Omega_f_sr [1/second]
+        10: Omega_f_ar [1/second]
+        11: U_0_sr
+        12: Uar
+            
+
+    
     '''
+    
+    # ------------- UNPACK PARAMS -------------
+    # Unpack Neuron parameters
+    sigma_ = params[0]
+    g_AHP_ = params[1]
+    delta_ = params[2]
+    I_inj_ = params[3]
+    
+    # Unpack Synapse parameters
+    Xi_ampa_ =   params[4]
+    Xi_nmda_ = params[5]
+    Y_T_ = params[6]
+    conn_prob_ = params[7]
+    Omega_d_ = params[8]
+    Omega_f_sr_ = params[9]
+    Omega_f_ar_ = params[10]
+    U_0_sr_ = params[11]
+    Uar_ = params[12]
+    
+    
     
     # Clear the cache for the 'cython' code generation target
     # clear_cache('cython') 
+    BrianLogger.suppress_hierarchy('brian2.devices')
+    BrianLogger.suppress_hierarchy('brian2.parsing')
+
     start_scope()
-    # ------------------------- SET OPTIONS -------------------------
-
-    # ------- Synapses -------
-    synapse_type='neutral'
-    ics=None 
-    dt=None            
-
-    postc_sic='double-exp'
-    Decay_type = 'Double_exp'
-    sic=None 
-    delay=None
-    RandomKinetics = False 
-    OnlyExc = True
-    std_pers =0.01
-
-    Syn_Currents_model = 'TM-coupled' # 'Kinetic','Nina','TM-coupled'
-
-    Max_delay = 25 *ms
-    add_delay = False
-    delay_mode = 'random'
-
-
-
-    # ------- Neurons -------
-    Adaptation = True
-
+    
 
     # ------------------------- PARAMETERS -------------------------
 
     # --------- SIMULATION -----------
-    simtime = 10 * second               # simulation time
+    simtime = 10 * second               # simulation time must be coherent with the 1D_CNN training one.
     # transient = 3 * second              # time omitted as transient
     sed = 39                             # random number seed
     devices.device.seed(sed)            # set the seed for all the random number realisations
-
+    Simulated_network = 'Neuronal'
     # --------- NEURON -----------
     Nn = 100
     neuron_radius = 9 #[um]
-    # --------- SYNAPTIC -----------
-
-    # ----- Connectivity -----
-    # Can be directly an ADJ or a string: Random, Distance
-    # Connection_neuro = np.array(([0,1,0,0],
-    #                               [0,0,1,0],
-    #                               [0,0,0,1],
-    #                               [0,0,0,0]))
-    Connection_neuro = 'Distance'
+    #
 
    
 
@@ -313,25 +345,146 @@ def Network_wrapper():
     # ------------------------- GROUPS BUILD-UP -------------------------
 
 
+    # --------- NEURON and SYNAPSE -----------
+    N,S = Neuronal_Network(Nn,Connection_var = 'Random', 
+                        add_delay= False,delay_mode= 'random',
+                         Max_Delay = 10*ms,ics = False, Simulated_network = 'Neuronal',
+                         Decay_type = 'Double_exp',synapse_type = 'neutral',sed=sed)
+    
+    
+    
+    # ------------- UPDATE PARAMETERS -------------
+    N.namespace['sigma'] = sigma_*mV
+    N.namespace['g_AHP'] = (g_AHP_*msiemens*cm**-2) * N.namespace['area']
+    N.namespace['delta'] = delta_
+    N.namespace['I_inj'] = I_inj_ * pA
+    
+    S.namespace['Xi_ampa'] = Xi_ampa_ / mmole
+    S.namespace['Xi_nmda'] = Xi_nmda_ / mmole
+    S.namespace['Y_T'] = Y_T_ * mmole
+    S.namespace['conn_prob_'] = conn_prob_
+    S.namespace['Omega_d'] = Omega_d_ / second
+    S.namespace['Omega_f_sr'] = Omega_f_sr_ / second
+    S.namespace['Omega_f_ar'] = Omega_f_ar_ / second
+    S.namespace['U_0_sr'] = U_0_sr_
+    S.namespace['Uar'] = Uar_
+    
+    
+    # Update the externally injected current
+    N.I = '(rand() -0.5) * I_inj'          # Make neurons heterogeneously excitable
+    
         
-        
-        
-    Simulated_network == 'Neuronal':
+    MonitorN = StateMonitor(N, ['V','I_cell'], record=True)
+    net_ = Network(collect())  # automatically include all the stated groups
+    net_.run(simtime)
 
-        # --------- NEURON and SYNAPSE -----------
-        N,S = Neuronal_Network(Nn,Connection_neuro, RandomKinetics=RandomKinetics, OnlyExc=OnlyExc,
-                               Syn_Currents_model=Syn_Currents_model,add_delay=add_delay,
-                               delay_mode=delay_mode,Max_delay=Max_delay,ics=ics,
-                               std_pers=std_pers, Simulated_network=Simulated_network,Decay_type=Decay_type,synapse_type = synapse_type,Out_path = Out_path)
+    
+        
+    # --------------- ELECTRODE RECORDINGS ---------------
+    Traces,MEA_dict = Electrode_traces(pitch,pitch_recsites,shift,N,MonitorN,electrode_dist,neuron_radius,electrode_radius)
+    
+    clock_dt = defaultclock.dt 
+    fs = 1/(clock_dt/second)
+    Raster,Raster_array = get_Raster(Traces,fs)
+    
         
         
+   
+ 
+    return   MonitorN  ,N,S
+    
+neuron_radius = 9 #[um]
+#
+
+
+
+# --------- ELECTRODE RECORDINGS ----------- 
+pitch = 300 #[um] 
+electrode_radius = 15 #[um]
+
+pitch_recsites = 7.5 # [um]  
+shift = 11.25 # [um]
+
+electrode_dist = 300 # [um]
+
+c_min = 0 #[um]
+c_max = 1100 #[um]
+ 
+    
+ 
+
+
+
+
+
+    
+# Initialize a list with placeholder values.
+# The number of elements corresponds to the total number of parameters.
+params = [0.0] * 13
        
+# Update the values at the specific indices
+# Neuron
+params[0] = 4.1  # Sigma [mV]
+params[1] = 0.005  # g_AHP
+params[2] = 0.6  # delta
+params[3] = 10.0  # I_inj [pA]
+
+# Synapse
+params[4] = 0.5  # Xi_ampa [1/mmole]
+params[5] = 0.3  # Xi_nmda [1/mmole]
+params[6] = 500.0  # Y_T [mmole]
+params[7] = 0.10  # conn_prob
+params[8] = 3  # Omega_d [1/second]
+params[9] = 3  # Omega_f_sr [1/second]
+params[10] = 1.1428  # Omega_f_ar [1/second]
+params[11] = 0.6  # U_0_sr
+params[12] = 0.001 # Uar    
+
+    
+
+    
+MonitorN  ,N,S = Network_wrapper(params)
+
+# --------------- RUN NETWORK ---------------
+
+#%%
+
+
+    #%%
+
+
+#%%
+
+# ----- CUMULATIVE TRACES ------
+smoothed_cumulative,fs_downsampled = Neuronal_traces_simulation(Raster_array,fs = fs,Visible = True,w_size=0.02,Gaussian_window=0.04)
+
+
+# cumulative_stdz = Standardization(smoothed_cumulative)
+
+# data_array =  torch.unsqueeze(torch.from_numpy(cumulative_stdz),0).float32()
+
+
+    
+    # # ----- EMBEDDING NETWORK ------
+    # device = torch.device("cpu") # for multiprocessing 
+    
+    # # Load data to device 
+    # data_array = data_array.to(device)
+    
+    # # Load the model
+    # embedding_network = torch.load("C:\Users\Admin\Desktop\Leonardo\Summary Networks\Saved networks\250825\Main_Model_.pt")
+    # embedding_network = embedding_network.to(device)
+
+    # embedding_network.eval() # Always set the model to evaluation mode for inference
+    # with torch.no_grad():
         
+    #     # In embedding mode the network expects the input data to be (1,length)
+    #     Emb = embedding_network(data_array,fs_downsampled,State='Embedding')
+        
+        
+    # return Emb
 
         
-        
-    
-    
     
     
     
